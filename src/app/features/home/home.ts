8d3dfd767541d496ce586582f19546764d8a6df4
panchoxp <francisco_smfd@hotmail.es>
2026-08-12 18:10:07 -0500

    layout stiles

---
+++ src/app/features/home/home.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero/hero';
+@Component({
+  selector: 'app-home',
+  imports: [Hero],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {}
