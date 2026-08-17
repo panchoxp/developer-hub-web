89dceb70dc4aef0ca6b403001290f8bb93268827
panchoxp <francisco_smfd@hotmail.es>
2026-08-17 16:32:45 -0500

    skills

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero/hero';
 import { About } from './about/about';
+import { Skills } from './skills/skills';
 @Component({
   selector: 'app-home',
-  imports: [Hero, About],
+  imports: [Hero, About, Skills],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
