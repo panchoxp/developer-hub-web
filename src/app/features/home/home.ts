042dbee3a336e3e1851432f882b431fd7e1fa1a0
panchoxp <francisco_smfd@hotmail.es>
2026-08-16 19:23:56 -0500

    about

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero/hero';
+import { About } from './about/about';
 @Component({
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero, About],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
